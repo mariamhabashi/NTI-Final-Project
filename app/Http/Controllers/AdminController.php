75dcc858d73dc0714bb9cabedebc0aa43a62fa6e
Mohamed Ahmed Mohamed Abdallah <114699233+mohamed-ahamed-mohamed-2026@users.noreply.github.com>
2025-08-28 16:31:58 +0300

    Update AdminController.php
    Admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,35 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use app\Models\Doctor;
+use App\Models\Doctor;
 use Illuminate\Http\Request;
-
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        return view('admin.index', ['doctors' => Doctor::all()]);
     }
+
     public function create()
     {
         return view('admin.create');
     }
+
     public function store(Request $request)
     {
-        return view('admin.index');
-    }
-    public function edit(Doctor $doctor)
-    {
-        return view('admin.edit');
-    }
-    public function update(Doctor $doctor)
-    {
-        return view('admin.edit');
-    }
-    public function destroy(Doctor $doctor)
-    {
-        return view('admin.index');
+        Doctor::create($request->only(['first_name', 'last_name', 'specialty', 'city']));
+        return redirect()->route('admin.doctors.index');
     }
 
+    public function edit(Doctor $doctor)
+    {
+        return view('admin.edit', compact('doctor'));
+    }
+
+    public function update(Request $request, Doctor $doctor)
+    {
+        $doctor->update($request->only(['first_name', 'last_name', 'specialty', 'city']));
+        return view('admin.doctors.index', ['doctors' => Doctor::all()]);
+    }
+
+    public function destroy(Doctor $doctor)
+    {
+        $doctor->delete();
+        return redirect()->route('admin.doctors.index');
+    }
 }
+
